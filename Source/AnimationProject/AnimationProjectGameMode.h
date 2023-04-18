dff222169437a8ce06d937cc32963e04217fd140
Xiawen9 <1217459864@qq.com>
2023-04-19 00:19:44 +0800

    update

---
+++ Source/AnimationProject/AnimationProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnimationProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAnimationProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAnimationProjectGameMode();
+};
+
+
+
